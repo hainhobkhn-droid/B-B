969082f33af5905e7f378bc341ffbfd71c3c7fec
hainhobkhn-droid <hainhobkhn@gmail.com>
2026-09-16 23:12:06 +0700

    Add UI visual system and club branding

--- config.js
+++ config.js
@@ -1,5 +1,16 @@
 window.APP_CONFIG = {
-    SUPABASE_URL: 'https://bflwaqlvnesuqoyikxar.supabase.co',
+  SUPABASE_URL: 'https://bflwaqlvnesuqoyikxar.supabase.co',
 
-    SUPABASE_ANON_KEY: 'sb_publishable_NiFIbGkglgbBvlYVN_jXBw_CUW9Kw0r'
+  SUPABASE_ANON_KEY:
+    'sb_publishable_NiFIbGkglgbBvlYVN_jXBw_CUW9Kw0r',
+
+  APP_NAME: 'PICK',
+
+  CLUB_NAME: 'B&B Club',
+
+  CLUB_TAGLINE:
+    'Cùng chơi • Cùng tiến bộ',
+
+  CLUB_LOGO:
+    './logo.jpg'
 };
